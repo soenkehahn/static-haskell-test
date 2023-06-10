2cf8091b53c503454ee66073af8e094116c7b9dc
Sönke Hahn <soenkehahn@gmail.com>
2023-06-10 14:41:00 +0200

    Put in static hpack

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = {
+          default = pkgs.pkgsStatic.haskell.packages.ghc96.hpack;
+        };
+      }
+    );
+}
